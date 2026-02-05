38c0a53f47eff15ba722bfcad4b0cd0a02c67074
Ariel_Izralevitch <your.email@example.com>
2026-02-05 10:34:55 +0200

    in process

---
+++ app/dal.py
@@ -0,0 +1,67 @@
+from pymongo import MongoClient
+
+client = MongoClient("employee_data_advanced.json")
+
+db = client.test
+employees = db.employees
+
+
+def get_engineering_high_salary_employees ():
+    return list(employees.find({"job_role.department":"Engineering","salary":{"$gt":65000}},
+                                {"_id": 0,
+                                "employee_id":1,
+                                "name":1,
+                                "salary":1}))
+
+
+
+def get_employees_by_age_and_role():
+    return list(employees.find({{"age":{"$gte":30 ,"$lte":45}},{"job_role.title":{"$in":["Specialist","Engineer"]}}}
+                               ,{"_id": 0}))
+
+
+
+def get_top_seniority_employees_excluding_hr():
+    return list(employees.find({"job_role.department":{"$ne":"HR"}},
+                               {"_id": 0}).sort("years_at_company",-1).limit(7))
+
+
+
+def get_employees_by_age_or_seniority():
+    return list(employees.find({"$or":[{"age":{"$gt":50}},{"years_at_company":{"$lt":3}}]}
+                                ,{"_id": 0,
+                                "employee_id":1,
+                                "name":1,
+                                "age":1,
+                                "years_at_company":1}))
+
+
+
+def get_managers_excluding_departments():
+    return list(employees.find({{"job_role.title":"Manager"},{"job_role.department":{"nin":["Sales","Marketing"]}}}
+                               ,{"_id": 0}))
+
+
+def get_employees_by_lastname_and_age():
+    return list(employees.find({{"age":{"$lt":35}},{"name":{"$regex": "ces$"}}
+
+
+
+
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from dal import *
+
+app = FastAPI()
+
+
+
+@app.get("/employees/engineering/high-salary")
+def employees_engineering_high_salary():
+    return get_engineering_high_salary_employees()
+
+
+
+@app.get("/employees/by-age-and-role")
+def employees_by_age_and_role():
+    return get_employees_by_age_and_role()
+
+
+@app.get("/employees/top_seniority")
+def employees_top_seniority():
+    return get_top_seniority_employees_excluding_hr()
+
+
+@app.get("/employees/age-or-seniority")
+def employees_age_or_seniority():
+    return get_employees_by_age_or_seniority()
+
+
+@app.get("/employees/managers/excluding-departments")
+def employees_managers_excluding_departments():
+    return get_managers_excluding_departments()
+
+
+@app.get("/employees/by-lastname-and-age")
+def employees_by_lastname_and_age():
+    return get_employees_by_lastname_and_age()
+
+
+
+
+
+
+
